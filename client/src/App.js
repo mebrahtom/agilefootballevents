93d37f278a12d10bec460ee24e379fb54847338e
Rasmus Davidsson <ralle.davidsson@gmail.com>
2018-03-30 21:26:32 +0200

    Changed result, upcoming games and groups tab design and added content on groups tab

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './img/logo.png';
 import './App.css';
 
-import { Button, Row, Col, Navbar, Nav, NavItem} from 'react-bootstrap';
+import { Button, Row, Col, Navbar, Nav, NavItem, Tabs, Tab, Table} from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -27,13 +27,13 @@ class Header extends Component {
         </Navbar.Header>
         <Navbar.Collapse className="app-header navbar-collapse-custom">
           <Nav pullRight className="nav-custom">
-            <NavItem eventKey={1} href="#" >
+            <NavItem eventKey={1} href="#home" >
               Home
             </NavItem>
-            <NavItem eventKey={2} href="#">
+            <NavItem eventKey={2} href="#football">
               Football
             </NavItem>
-            <NavItem eventKey={2} href="#">
+            <NavItem eventKey={2} href="#exploration">
               Explore gothenburg
             </NavItem>
           </Nav>
@@ -53,29 +53,74 @@ function renderMatches() {
   return matches;
 }
 
+function renderGroups() {
+  var groups = [];
+
+  for(var i = 0; i < 4; i++){
+
+    groups.push(<GroupTable/>);
+  }
+
+  return groups;
+}
+
+function renderGroupRows() {
+
+  var grouprow = [];
+
+  for(var i = 0; i < 6; i++){
+    grouprow.push(
+      <tr>
+        <td>Team Name</td>
+        <td>#Won games</td>
+        <td>#Draws</td>
+        <td>#Losses</td>
+        <td>#Points</td>
+      </tr>
+    );
+  }
+
+  return grouprow;
+}
+
 class MatchTable extends Component {
   render() {
     const matches = renderMatches();
+    const groups = renderGroups();
 
     return (
         <div class ="container">
-          <div class="selection-menu" >
+          <Tabs defaultActiveKey={1}>
+            <Tab eventKey={1} title="Results">
+              { /*Result Content Here */}
+              <div className="match-container">
+                { matches }
+                <div className="center-hz">
+                  <Button>Show more</Button>
+                </div>
+              </div>
+              
+            </Tab>
+            <Tab eventKey={2} title="Upcoming Games">
+              {/* Upcoming Games content here*/}
+            </Tab>
+            <Tab eventKey={3} title="Groups">
+              {/* Group content here*/}
+              { groups }
+            </Tab>
+          </Tabs>
+          {/*<div class="selection-menu" >
             <div className="selection-item selection-item-select">
               <span>Results</span>
             </div>
-            <div className="selection-item">
-              <span >Upcoming games</span>
+            <div className="selection-item" href="#item">
+              <span href="#item">Upcoming games</span>
             </div>
             <div className="selection-item">
               <span>Tables</span>
             </div>
-          </div>
-          <div className="match-container">
-            { matches }
-            <div className="center-hz">
-              <Button>Show more</Button>
-            </div>
-          </div>
+          </div>*/}
+          
         </div>
     );
   }
@@ -101,6 +146,35 @@ class MatchRow extends Component {
             Location
           </Col>
         </Row>
+    );
+  }
+}
+
+class GroupTable extends Component {
+  render() {
+    const grows = renderGroupRows();
+    return(
+      <Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <th>Team</th>
+            <th>W</th>
+            <th>D</th>
+            <th>L</th>
+            <th>P</th>
+          </tr>
+        </thead>
+      <tbody>
+        { grows }
+        {
+
+        /*<tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>*/}
+      </tbody>
+      </Table>
     );
   }
 }
