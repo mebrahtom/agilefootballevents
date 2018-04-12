bf3625a56ca1645c3877787283430b9581dd433e
Filip Larsson <filip.larsson.vbg@gmail.com>
2018-04-12 10:29:05 +0200

    engaging connection with db in dbconnection. Just need to import then query is possible

--- api/config/dbconnection.js
+++ api/config/dbconnection.js
@@ -1,8 +1,15 @@
-var mysql=require('mysql');
-const conn=mysql.createConnection({
+var mysql = require('mysql');
+
+const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:"",
     database:'db1'
 });
-module.exports=conn;
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+module.exports = connection;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var conn=require('../config/dbconnection');
+var express = require('express')
+var router = express.Router()
+var db = require('../config/dbconnection')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+  res.render('index', { title: 'Express' })
+})
 
 router.get('/checkconnection',function(req,res){
-    conn.connect((err)=>{
-        if(err) throw err;
-        res.send("Connected!");
-    });
-});
 
-module.exports = router;
+})
+
+module.exports = router
