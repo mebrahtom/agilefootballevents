77d3ae7ea6e5175180fe1a352aab68a354b9c035
mebrahtom <mebrinno@gmail.com>
2018-04-12 09:26:01 +0200

    Testing db connection

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var conn=require('../config/dbconnection');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/checkconnection',function(req,res){
+    conn.connect((err)=>{
+        if(err) throw err;
+        res.send("Connected!");
+    });
 });
 
 module.exports = router;
