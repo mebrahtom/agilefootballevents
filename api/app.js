c6e5b75b6f0b51c84f3a647551c23c79cd83863b
Filip Larsson <filip.larsson.vbg@gmail.com>
2018-04-12 11:27:55 +0200

    Created env files so no need to store passwords publically

--- api/app.js
+++ api/app.js
@@ -1,10 +1,10 @@
+var dontenv = require('dotenv').config({path: __dirname + '/../.env'})
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 
--- api/config/dbconnection.js
+++ api/config/dbconnection.js
@@ -1,15 +1,17 @@
 var mysql = require('mysql');
+const pw = process.env.DB_PASSWORD
+console.log("Password is", pw)
 
 const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:"",
-    database:'gothenballdb'
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'db1'
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+  if (err) throw err
+  console.log("Connected!")
+})
 
-module.exports = connection;
+module.exports = connection
