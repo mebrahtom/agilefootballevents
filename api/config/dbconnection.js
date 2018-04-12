524e9aef4a350cb3d103f7d7e390c7648aed9865
Mebrahtom Guesh <mebrinno@gmail.com>
2018-04-12 10:35:10 +0200

    Update dbconnection.js

--- api/config/dbconnection.js
+++ api/config/dbconnection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:"",
-    database:'gothenballdb'
+    database:'gothencupdb'
 });
 
 connection.connect(function(err) {
