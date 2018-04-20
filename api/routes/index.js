56600a97f6e25aa44ee93f5907fa34f09567e669
Rasmus Davidsson <ralle.davidsson@gmail.com>
2018-04-20 15:24:03 +0200

    bug

--- api/routes/index.js
+++ api/routes/index.js
@@ -32,14 +32,6 @@ router.get('/fixtures/playedMatches', function(req, res, next) {
   })
 })
 
-router.get('/fixtures/groups1', function(req, res, next) {
-  var sql = 'SELECT groupName, GROUP_CONCAT(CONCAT({team:\"\', team, \'\"})) list FROM HelperResultTable GROUP BY groupName;'
-  db.query(sql, function(err, result, fields) {
-    if (err) throw err
-    res.json(result)
-
-  })
-
 router.get('/fixtures/matchfixtures', function(req, res, next) {
   var sql = 'SELECT * FROM MatchFixtures'
   db.query(sql, function(err, result, fields) {
