1824f7f7ba32060be000f9f117536b3f68909c86
Joseph <joeadcock94@hotmail.com>
2018-04-24 15:21:37 +0200

    Added group field to form

--- client/src/containers/AdminPage.js
+++ client/src/containers/AdminPage.js
@@ -35,6 +35,9 @@ class UpdateForm extends Component {
 
   getValidation(e){
     // Handle validation for team names 
+    if(this.state.teamOne === "" || this.state.teamTwo === "") return null;
+    else if (this.state.teamOne === this.state.teamTwo) return 'error';
+    return null;
   }
 
 
@@ -43,32 +46,50 @@ class UpdateForm extends Component {
       <div name = "resultForm">
         <Form horizontal className="formUpdate">
         <Col><h3>Match Results</h3></Col>
-          <FormGroup controlId="formHorizontalTeam">
-            <Col componentClass={ControlLabel} sm={4}>
+          <FormGroup controlId="formHorizontalTeam" validationState={this.getValidation()}>
+            <Col componentClass={ControlLabel} sm={7} >
               Team 1
             </Col>
-            <Col sm={8}>
+            <Col sm={7} smOffset={3}>
              <FormControl type = "text" placeholder="Name: " name="teamOne" 
-             value={this.state.teamOne} onChange={this.handleChange}/>
+             value={this.state.teamOne} onChange={this.handleChange} />
              <FormControl type = "number" placeholder="Goals:" 
-             name="goalsOne" value={this.state.goalsOne} onChange={this.handleChange} min="0"/>
+             name="goalsOne" value={this.state.goalsOne} onChange={this.handleChange} min="0" sm={6}/>
             </Col>
           </FormGroup> 
 
-          <FormGroup controlId="formHorizontalTeam">
-            <Col componentClass={ControlLabel} sm={4}>
+          <FormGroup controlId="formHorizontalTeam" validationState={this.getValidation()}>
+            <Col componentClass={ControlLabel} sm={7}>
               Team 2
             </Col>
-            <Col sm={8}>
+            <Col sm={7} smOffset={3}>
               <FormControl type="text" placeholder="Name:" name="teamTwo" 
               value={this.state.teamTwo} onChange={this.handleChange}/>
               <FormControl type="number" placeholder="Goals:" name="teamTwo" 
               value={this.state.goalsTwo} onChange={this.handleChange} min="0"/>
             </Col>
+
+            <Col componentClass={ControlLabel} sm={7}>
+            Group:
+            </Col>
+            <Col sm={7} smOffset={3}> 
+            <FormControl componentClass="select" placeHolder="Group:">
+             <option value="A">A</option>
+             <option value="B">B</option>
+             <option value="C">C</option>
+             <option value="D">D</option>
+             <option value="E">E</option>
+             <option value="F">F</option>
+             <option value="G">G</option>
+             <option value="H">H</option>
+             <option value="I">I</option>
+             </FormControl>
+            </Col>
           </FormGroup>
+          
 
           <FormGroup>
-            <Col smOffset={4} sm={8}>
+            <Col smOffset={2} sm={8}>
               <Button type="submit">Update</Button>
             </Col>
           </FormGroup>
