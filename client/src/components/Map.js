35e3a212e5d8f5af4141b73cec6165daf969412c
Jonas Åkesson <jonas.akesson1@gmail.com>
2018-04-26 14:53:59 +0200

    Changed google map react component

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,29 +1,59 @@
-import React, {Component} from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { Component } from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
+const style = {
+  width: '100%',
+  height: '100vh', 
+}
 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 57.7089,
-      lng: 11.9746
-    },
-    zoom: 13
+export class GoogleMap extends Component {
+  
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
   };
-
+  
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+ 
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+  
   render() {
     return (
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyDgDe9NgUX5v-I8MQ2IM0YkERnKEgnc_NM'}}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          
-        </GoogleMapReact>
-      </div>
+      <Map 
+        google={this.props.google} 
+        zoom={15}
+        style={style} 
+        initialCenter={{
+          lat: 57.7059,
+          lng: 11.9873
+        }}
+      >
+        <Marker onClick={this.onMarkerClick} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h2>Ullevi</h2>
+            </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
-
-export default SimpleMap;
+ 
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyB0zQvbfo-wBPQWPV_rBwUo3FRQI-O7aEY')
+})(GoogleMap)
