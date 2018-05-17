b48c1223e5cad10a92dcab3ca835edf3c79b92e4
mebrahtom <mebrinno@gmail.com>
2018-05-17 11:46:15 +0200

    Admin session tracked, logout link and function added

--- client/src/containers/AdminPage.js
+++ client/src/containers/AdminPage.js
@@ -3,17 +3,14 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import InsertResult from '../components/InsertResult'
-/*import { Redirect } from 'react-router';*/
-
+import {Route,Redirect} from 'react-router-dom'
+import store from '../store'
 class AdminPage extends Component {
   render() {
-
-
-    /*
-    Should add this. But we have no way to check if user is logged in yet
-    if (this.props.admin === '') {
-      return <Redirect push to="/login" />;
-    }*/
+    const token=localStorage.getItem('jwtToken');
+    if(token===undefined){
+      return <Redirect to="/login" />
+    }
     return (
       <div>
         <InsertResult />
